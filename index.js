8f5ea40b7c84264ea3f248d9dc7474f4d2810731
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-07-25 18:01:40 +0300

    add TextBox component

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Image from './src/components/Image';
+import TextBox from './src/components/TextBox';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Image url={'http://via.placeholder.com/200x200'} width={200} heigh={200} />
+                <TextBox>Some text</TextBox>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <Image url={'http://via.placeholder.com/200x200'} width={200} heigh={200} />,
+    <App />,
     document.getElementById('root')
 )
